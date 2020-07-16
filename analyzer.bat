9fe685842bda30683360c40d8bd4203b11ce1fc3
Tony Graham <tgraham@antenna.co.jp>
2020-07-16 22:25:48 +0100

    Log file for PDFs now '*.pdf.log'. More comparing file dates with date of AHFCmd.exe file. Working around '(86)' in %AHF70_HOME% value causing problems with DOS statements. Better handling of spaces in file and directory names.

--- analyzer.bat
+++ analyzer.bat
@@ -154,15 +154,15 @@ goto initial
 
 set FILE=%d%
 
-if not exist %d% (
+if not exist "%d%" (
    echo.
-   echo Source file does not exist: %d%
+   echo Source file does not exist: '%d%'
    goto error
 )
 set BASENAME=
-for /F  %%I in ("%FILE%") do set BASENAME=%%~nI
+call :sub_basename BASENAME "%FILE%"
 
-rem echo %FILE%%BASENAME%
+rem echo '%FILE%' '%BASENAME%'
 
 if not "%BASENAME%"=="" goto have_basename
 echo Could not determine basename for %FILE%
@@ -193,29 +193,42 @@ if not "%xslt%"=="" (
 
 if not "%ahfcmd%"=="" (
    if EXIST "%ahfcmd%" (
-      set AHFCMD=%ahfcmd%
+      set AHFCMD="%ahfcmd%"
+      goto ahfcmd_done
    ) else (
       echo AHFCmd file does not exist: %ahfcmd%
       goto error
    )
 ) else if not "%AHF70_64_HOME%"=="" (
    if EXIST "%AHF70_64_HOME%\AHFCmd.exe" (
-      set AHFCMD=%AHF70_64_HOME%\AHFCmd.exe
+      set AHFCMD="%AHF70_64_HOME%\AHFCmd.exe"
+      goto ahfcmd_done
    ) else (
       echo AHFCmd file does not exist in '%AHF70_64_HOME%'
       goto error
    )
-) else if not "%AHF70_HOME%"=="" (
-   if EXIST "%AHF70_HOME%\AHFCmd.exe" (
-      set AHFCMD=%AHF70_HOME%\AHFCmd.exe
-   ) else (
-      echo AHFCmd file does not exist in '%AHF70_HOME%'
-      goto error
-   )
-) else (
+)
+
+rem The '(86)' in %AHFXX_HOME% value causes problem when %AHFXX_HOME%
+rem is used in an 'if' statement that includes '(' and ')'.  It is
+rem necessary to do the check a different way.
+if "%AHF70_HOME%"=="" (
   echo No AHFCmd.exe
   goto error
 )
+
+if EXIST "%AHF70_HOME%\AHFCmd.exe" goto ahfcmd_ahfxx_home
+
+echo AHFCmd file does not exist in '%AHF70_HOME%'
+goto error
+
+:ahfcmd_ahfxx_home
+
+set AHFCMD="%AHF70_HOME%\AHFCmd.exe"
+
+:ahfcmd_done
+
+rem echo %AHFCMD%
 
 rem Check format value before taking time to generate Area Tree XML.
 if not "%format%"=="annotate" (
@@ -227,52 +240,50 @@ if not "%format%"=="annotate" (
    )
 )
 
-rem All of the reasons to generate %BASENAME%.AT.XML...
+rem All of the reasons to generate "%BASENAME%.AT.XML" ...
 
-if "%force%"=="yes" goto at
+if "%force%"=="yes" goto at_do
 
-if not exist %BASENAME%.AT.XML goto at
+if not exist "%BASENAME%.AT.XML" goto at_do
 
-for /F "usebackq" %%q in (`dir /B /OD %FILE% %BASENAME%.AT.XML`) do (
+rem Sort files by date. Newer file is last to set NEWER.
+for /F "usebackq delims=" %%q in (`dir /B /OD "%FILE%" "%BASENAME%.AT.XML"`) do (
     set NEWER=%%q
 )
 rem echo %NEWER%
 
-if not "%NEWER%"=="%BASENAME%.AT.XML" goto at
+if not "%NEWER%"=="%BASENAME%.AT.XML" goto at_do
 
-FOR %%? IN ("%AHFCMD%") DO (
-    set AHFCMD_DATE=%%~t?
-)
+rem 'dir /B /OD' does not work across directories.
+rem Use a function that returns timestamp in seconds since 1970.
+if exist "%BASENAME%.AT.XML" call :sub_file_mod_time "%BASENAME%.AT.XML" AT_XML_DATE
+call :sub_file_mod_time %AHFCMD% AHFCMD_DATE
 
-FOR %%? IN ("%BASENAME%.AT.XML") DO (
-    set AT_XML_DATE=%%~t?
-)
-
-if "%AHFCMD_DATE%" GTR "%AT_XML_DATE%" goto at
+if "%AHFCMD_DATE%" GTR "%AT_XML_DATE%" goto at_do
 
 rem No need to regenerate %BASENAME%.AT.XML
-echo %BASENAME%.AT.XML is up to date
+echo '%BASENAME%.AT.XML' is up to date
 
 goto at_done
 
-:at
+:at_do
 
 echo Analyzing '%FILE%' and generating '%BASENAME%.AT.XML'
 
-if exist %BASENAME%.AT.XML del %BASENAME%.AT.XML
-if exist %BASENAME%.AT.XML (
+if exist "%BASENAME%.AT.XML" del "%BASENAME%.AT.XML"
+if exist "%BASENAME%.AT.XML" (
    echo Unable to replace '%BASENAME%.AT.XML'.
    goto error
 )
 
-"%AHFCMD%" -analyze -p @AreaTree -xmlerr -d %FILE% %opt% -o %BASENAME%.AT.XML 2> %BASENAME%.log.xml
+%AHFCMD% -analyze -p @AreaTree -xmlerr -d "%FILE%" %opt% -o "%BASENAME%.AT.XML" 2> "%BASENAME%.log.xml"
 
 if %ERRORLEVEL% NEQ 0 (
    echo An error occurred when analyzing '%FILE%'. Check log file '%BASENAME%.log.xml'.
    goto error
 )
 
-if not exist %BASENAME%.AT.XML (
+if not exist "%BASENAME%.AT.XML" (
    echo Area Tree XML file for '%FILE%' was not created. Check log file '%BASENAME%.log.xml'.
    goto error
 )
@@ -345,17 +356,17 @@ if %ERRORLEVEL% NEQ 0 (
    goto error
 )
 
-call :sub_2pdf %PWD%\%BASENAME%.annotated.AT.xml %PWD%\%BASENAME%.annotated.pdf %PWD%\%BASENAME%.annotated.log.txt
+call :sub_2pdf "%PWD%\%BASENAME%.annotated.AT.xml" "%PWD%\%BASENAME%.annotated.pdf" "%PWD%\%BASENAME%.annotated.pdf.log"
 
 if %ERRORLEVEL% NEQ 0 goto error
 
 rem View the PDF
-if exist %BASENAME%.annotated.pdf (
+if exist "%BASENAME%.annotated.pdf" (
    echo Analysis completed.
    echo Annotated PDF: '%BASENAME%.annotated.pdf'
-   %BASENAME%.annotated.pdf 
+   "%BASENAME%.annotated.pdf"
 ) else (
-   echo An error occurred when generating '%BASENAME%.annotated.pdf'. Check log file '%BASENAME%.annotated.log.txt'.
+   echo An error occurred when generating '%BASENAME%.annotated.pdf'. Check log file '%BASENAME%.annotated.pdf.log'.
    goto error
 )
 
@@ -379,22 +390,29 @@ rem Whether or not to regenerate %BASENAME%.pdf...
 
 if "%force%"=="yes" goto report_pdf_do
 
-for /F "usebackq" %%q in (`dir /B /OD %BASENAME%.fo %BASENAME%.pdf`) do (
+for /F "usebackq delims=" %%q in (`dir /B /OD "%BASENAME%.fo" "%BASENAME%.pdf"`) do (
     set NEWER=%%q
 )
 rem echo %NEWER%
 
 if not "%NEWER%"=="%BASENAME%.pdf" goto report_pdf_do
 
-echo %BASENAME%.pdf is up to date
+rem 'dir /B /OD' does not work across directories.
+rem Use a function that returns timestamp in seconds since 1970.
+if exist "%BASENAME%.pdf" call :sub_file_mod_time "%BASENAME%.pdf" PDF_DATE
+call :sub_file_mod_time %AHFCMD% AHFCMD_DATE
+
+if "%AHFCMD_DATE%" GTR "%PDF_DATE%" goto report_pdf_do
+
+echo '%BASENAME%.pdf' is up to date
 goto report_pdf_done
 
 :report_pdf_do
 
-call :sub_2pdf %PWD%\%BASENAME%.fo %PWD%\%BASENAME%.pdf %PWD%\%BASENAME%.log.txt "%opt%"
+call :sub_2pdf "%PWD%\%BASENAME%.fo" "%PWD%\%BASENAME%.pdf" "%PWD%\%BASENAME%.pdf.log" "%opt%"
 
 if %ERRORLEVEL% NEQ 0 (
-   echo An error occurred when generating '%BASENAME%.pdf'. Check log file '%BASENAME%.log.txt'.
+   echo An error occurred when generating '%BASENAME%.pdf'. Check log file '%BASENAME%.pdf.log'.
    goto error
 )
 
@@ -402,14 +420,14 @@ if %ERRORLEVEL% NEQ 0 (
 
 rem Whether or not to regenerate %BASENAME%.report.fo...
 
-for /F "usebackq" %%q in (`dir /B /OD %BASENAME%.fo %BASENAME%.log.xml %BASENAME%.report.fo`) do (
+for /F "delims=" %%q in ('dir /B /OD "%BASENAME%.fo" "%BASENAME%.log.xml" "%BASENAME%.report.fo"') do (
     set NEWER=%%q
 )
 rem echo %NEWER%
 
 if not "%NEWER%"=="%BASENAME%.report.fo" goto report_fo_do
 
-echo %BASENAME%.report.fo is up to date
+echo '%BASENAME%.report.fo' is up to date
 goto report_fo_done
 
 :report_fo_do
@@ -421,10 +439,10 @@ if "%__SUB_ON_PATH_MATCH%"=="" (
    goto error
 )
 
-for %%? in (%BASENAME%.fo) do set FO_DATE=%%~t?
+call :sub_mod_date FO_DATE "%BASENAME%.fo"
 
 echo Generating '%BASENAME%.report.fo' from '%BASENAME%.AT.xml'
-java -jar %LIB_DIR%\saxon9he.jar -s:%PWD:\=/%/%BASENAME%.AT.xml -xsl:%REPORTER_XSLT:\=/% -o:%BASENAME%.report.fo logfile=file:///%PWD%/%BASENAME%.log.xml lang=%lang% file=%PWD:\=/%/%BASENAME%.fo file-date="%FO_DATE%" pdf-file=%PWD:\=/%/%BASENAME%.pdf %xsltparam%
+java -jar "%LIB_DIR%\saxon9he.jar" "-s:%PWD:\=/%/%BASENAME%.AT.xml" "-xsl:%REPORTER_XSLT:\=/%" "-o:%BASENAME%.report.fo" "logfile=file:///%PWD%/%BASENAME%.log.xml" lang=%lang% "file=%PWD:\=/%/%BASENAME%.fo" file-date="%FO_DATE%" "pdf-file=%PWD:\=/%/%BASENAME%.pdf" %xsltparam%
 
 if %ERRORLEVEL% NEQ 0 (
    echo.
@@ -437,31 +455,31 @@ if %ERRORLEVEL% NEQ 0 (
 
 rem Whether or not to regenerate %BASENAME%.report.pdf...
 
-for /F "usebackq" %%q in (`dir /B /OD %BASENAME%.report.fo %BASENAME%.report.pdf`) do (
+for /F "usebackq delims=" %%q in (`dir /B /OD "%BASENAME%.report.fo" "%BASENAME%.report.pdf"`) do (
     set NEWER=%%q
 )
 rem echo %NEWER%
 
 if not "%NEWER%"=="%BASENAME%.report.pdf" goto report_pdf_do
 
-echo %BASENAME%.report.pdf is up to date
+echo '%BASENAME%.report.pdf' is up to date
 goto report_pdf_done
 
 :report_pdf_do
 
-call :sub_2pdf %PWD%\%BASENAME%.report.fo %PWD%\%BASENAME%.report.pdf %PWD%\%BASENAME%.report.log.txt "-pdfver %pdfver%"
+call :sub_2pdf "%PWD%\%BASENAME%.report.fo" "%PWD%\%BASENAME%.report.pdf" "%PWD%\%BASENAME%.report.pdf.log" "-pdfver %pdfver%"
 
 if %ERRORLEVEL% NEQ 0 goto error
 
 :report_pdf_done
 
 rem View the PDF
-if exist %BASENAME%.report.pdf (
+if exist "%BASENAME%.report.pdf" (
    echo Analysis completed.
    echo Report PDF: '%BASENAME%.report.pdf'
-   %BASENAME%.report.pdf 
+   "%BASENAME%.report.pdf"
 ) else (
-   echo An error occurred when generating '%BASENAME%.report.pdf'. Check log file '%BASENAME%.report.log.txt'.
+   echo An error occurred when generating '%BASENAME%.report.pdf'. Check log file '%BASENAME%.report.pdf.log'.
    goto error
 )
 
@@ -475,49 +493,91 @@ rem echo %1
 set __SUB_ON_PATH_MATCH=%~$PATH:1
 exit /B 1
 
+:sub_basename
+rem echo %1 %2
+set "%~1=%~n2"
+exit /B 1
+
+:sub_basename_ext
+rem echo %1 %2
+set "%~1=%~nx2"
+exit /B 1
+
+:sub_mod_date
+rem echo %1 %2
+set "%~1=%~t2"
+exit /B 1
+
 :sub_msxsl
-msxsl %BASENAME%.AT.xml %REPORTER_XSLT% -o %BASENAME%.annotated.AT.xml logfile=%PWD%/%BASENAME%.log.xml lang=%lang% %xsltparam%
+msxsl "%BASENAME%.AT.xml" "%REPORTER_XSLT%" -o "%BASENAME%.annotated.AT.xml" logfile="%PWD%/%BASENAME%.log.xml" lang=%lang% %xsltparam%
 exit /B %ERRORLEVEL%
 
 :sub_xsltproc
-xsltproc --stringparam logfile %PWD%/%BASENAME%.log.xml --stringparam lang %lang% %xsltparam% -o %BASENAME%.annotated.AT.xml %REPORTER_XSLT% %PWD%/%BASENAME%.AT.xml
+xsltproc --stringparam logfile "%PWD%/%BASENAME%.log.xml" --stringparam lang %lang% %xsltparam% -o "%BASENAME%.annotated.AT.xml" "%REPORTER_XSLT%" "%PWD%/%BASENAME%.AT.xml"
 exit /B %ERRORLEVEL%
 
 :sub_saxon6
-java -jar %LIB_DIR%\saxon.jar -o %BASENAME%.annotated.AT.xml %PWD:\=/%/%BASENAME%.AT.xml %REPORTER_XSLT:\=/% logfile=file:///%PWD%/%BASENAME%.log.xml lang=%lang% %xsltparam%
+java -jar "%LIB_DIR%\saxon.jar" -o "%BASENAME%.annotated.AT.xml" "%PWD:\=/%/%BASENAME%.AT.xml" "%REPORTER_XSLT:\=/%" "logfile=file:///%PWD%/%BASENAME%.log.xml" lang=%lang% %xsltparam%
 exit /B %ERRORLEVEL%
 
 :sub_saxon9
-java -jar %LIB_DIR%\saxon9he.jar -s:%PWD:\=/%/%BASENAME%.AT.xml -xsl:%REPORTER_XSLT:\=/% -o:%BASENAME%.report.fo logfile=file:///%PWD%/%BASENAME%.log.xml lang=%lang% %xsltparam%
+java -jar "%LIB_DIR%\saxon9he.jar" "-s:%PWD:\=/%/%BASENAME%.AT.xml" "-xsl:%REPORTER_XSLT:\=/%" "-o:%BASENAME%.report.fo" "logfile=file:///%PWD%/%BASENAME%.log.xml" lang=%lang% %xsltparam%
 exit /B %ERRORLEVEL%
 
 :sub_2pdf
 
-set SOURCE=%1
-set TARGET=%2
-set LOG=%3
+set SOURCE=%~1
+set TARGET=%~2
+set LOG=%~3
 set PDF_OPT=%~4
 
 rem If the PDF is open in Acrobat, it can't be either deleted or replaced.
-if exist %TARGET% del %TARGET%
-if exist %TARGET% (
+if exist "%TARGET%" del "%TARGET%"
+if exist "%TARGET%" (
    echo Unable to replace '%TARGET%'.
    exit /B 1
 )
 
 set SOURCE_BASENAME=
-for /F  %%I in ("%SOURCE%") do set SOURCE_BASENAME=%%~nxI
-
+call :sub_basename_ext SOURCE_BASENAME "%SOURCE%"
 
 set TARGET_BASENAME=
-for /F  %%I in ("%TARGET%") do set TARGET_BASENAME=%%~nxI
+call :sub_basename_ext TARGET_BASENAME "%TARGET%"
 
 echo Formatting '%SOURCE_BASENAME%' as '%TARGET_BASENAME%'
 rem Use highest-possible PDF version in case embedding high-version PDF
-"%AHFCMD%" -x 4 %PDF_OPT% -d %SOURCE% -o %TARGET% 2> %LOG%
+%AHFCMD% -x 4 %PDF_OPT% -d "%SOURCE%" -o "%TARGET%" 2> "%LOG%"
 
 if %ERRORLEVEL% NEQ 0 (
    echo An error occurred when generating '%TARGET%'. Check log file '%LOG%'.
    exit /B 1
 )
 exit /B 0
+
+rem From https://www.dostips.com/forum/viewtopic.php?p=38229#p38229
+:sub_file_mod_time  File  [TimeVar]
+::
+::  Computes the Unix time (epoch time) for the last modified timestamp for File.
+::  The result is an empty string if the file does not exist. Stores the result
+::  in TimeVar, or prints the result if TimeVar is not specified.
+::
+::  Unix time = number of seconds since midnight, January 1, 1970 GMT
+::
+setlocal disableDelayedExpansion
+:: Get full path of file (short form if possible)
+for %%F in ("%~1") do set "file=%%~sF"
+:: Get last modified time in YYYYMMDDHHMMSS format
+set "time="
+for /f "skip=1 delims=,. tokens=2" %%A in (
+  'wmic datafile where name^="%file:\=\\%" get lastModified /format:csv 2^>nul'
+) do set "ts=%%A"
+set "ts=%ts%"
+:: Convert time to Unix time (aka epoch time)
+if defined ts (
+  set /a "yy=10000%ts:~0,4% %% 10000, mm=100%ts:~4,2% %% 100, dd=100%ts:~6,2% %% 100"
+  set /a "dd=dd-2472663+1461*(yy+4800+(mm-14)/12)/4+367*(mm-2-(mm-14)/12*12)/12-3*((yy+4900+(mm-14)/12)/100)/4"
+  set /a "ss=(((1%ts:~8,2%*60)+1%ts:~10,2%)*60)+1%ts:~12,2%-366100-%ts:~21,1%((1%ts:~22,3%*60)-60000)"
+  set /a "ts=ss+dd*86400"
+)
+:: Return the result
+endlocal & if "%~2" neq "" (set "%~2=%ts%") else echo(%ts%
